ea0366d1f1572b30f69006816171a30087af5bec
root <audiocc@126.com>
2025-03-13 21:38:27 +0800

    updatre

---
+++ run.sh
@@ -0,0 +1,10 @@
+cd /root/www/papers/arxiv
+
+export GROBID_URLS=https://qingxu98-grobid.hf.space,https://qingxu98-grobid2.hf.space,https://qingxu98-grobid3.hf.space,https://qingxu98-grobid4.hf.space,https://qingxu98-grobid5.hf.space,https://qingxu98-grobid6.hf.space,https://qingxu98-grobid7.hf.space,https://qingxu98-grobid8.hf.space
+export SAVE_ROOT="../Papers"
+export ZOTERO_KEY="koz1F8Ij0ROWBghnRtejd4e8"
+export GROUP_ID="5262725"
+export USER_ID=
+
+/usr/bin/python3 main.py
+
